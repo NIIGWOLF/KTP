b6c64bc6b6d4628816e781ab88301c866702ec67
KlyushkinD <nigwolf10@gmail.com>
2020-05-07 18:10:43 +0300

    KTP lab7

---
+++ КТП/Лабораторные работы/lab7/src/Socket.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Socket {
+    java.net.Socket socket;
+
+    Socket (String host, int port) throws IOException {
+        socket = new java.net.Socket(host,port);
+        socket.setSoTimeout(5000);
+    }
+
+    void setSoTimeout(int timeout)throws IOException {
+        socket.setSoTimeout(timeout);
+    }
+
+    InputStream getInputStream()throws IOException {
+        return socket.getInputStream();
+    }
+
+    OutputStream getOutputStream()throws IOException{
+        return socket.getOutputStream();
+    }
+
+    void close()throws IOException{
+        socket.close();
+    }
+}
